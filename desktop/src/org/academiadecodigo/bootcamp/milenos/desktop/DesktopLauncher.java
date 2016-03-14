05fc10ff948bb5ddc627afb454c3ed5f96335e1c
milebza <miibaeza@gmail.com>
2016-03-14 19:32:21 +0000

    first commitgit add *git add *

---
+++ desktop/src/org/academiadecodigo/bootcamp/milenos/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.bootcamp.milenos.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.bootcamp.milenos.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
